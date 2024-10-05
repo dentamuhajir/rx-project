37ff3c15df5f9235d564c294298c9cf46e18ed6b
Denta Muhajir <dentamuhajir@gmail.com>
2024-10-05 20:48:17 +0700

    Create FeatureProduct css & embeeding on component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import FeaturedProducts from './components/FeaturedProducts/FeaturedProducts';
 import Header from './components/Header/Header';
 
 function App() {
   return (
     <div>
       <Header/>
+      <FeaturedProducts/>
     </div>
     
   );
