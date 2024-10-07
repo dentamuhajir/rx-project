a75ed50d145c7dfa42a541d8a24f2bed98cb9ddd
Denta Muhajir <dentamuhajir@gmail.com>
2024-10-07 15:07:09 +0700

    Create about component

---
+++ src/components/About/About.js
@@ -0,0 +1,13 @@
+// components/About/About.js
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>We are an eCommerce platform offering a wide variety of products...</p>
+    </div>
+  );
+}
+
+export default About;
